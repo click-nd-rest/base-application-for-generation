f8bab6bb5646043d8504c140950c9331049cdedd
straightth <speciallyfororigin@gmail.com>
2022-12-04 17:54:18 +0300

    Removing proto controller from context

--- base-crud-app/src/main/java/com/github/click/nd/rest/base/controller/ResourceController.java
+++ base-crud-app/src/main/java/com/github/click/nd/rest/base/controller/ResourceController.java
@@ -9,7 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+//@RestController
 @Validated
 @RequestMapping("/resource")
 @RequiredArgsConstructor
